a3d8924a6611de196a75b3dcc48a59bd9f2f36ec
qiaoxiyang <690734540@qq.com>
2015-12-16 17:49:31 +0800

    Initial Commit

---
+++ swiftNongJiBang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftNongJiBang
+//
+//  Created by lulei on 15/12/16.
+//  Copyright (c) 2015年 xiyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
